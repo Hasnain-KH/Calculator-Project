a686dbf7509ba449664fb2ef47879ae3a940f64d
Muhammadhasnain <webdevhasnain@gmail.com>
2026-05-11 22:21:47 +0500

    update

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ btn.forEach((btn) => {
     btn.addEventListener("click", function (detail) {
         if (detail.target.value === "=") {
             let save = eval(display.value);
-            display.value = save;
-
+            display.value = display.value + "\n" + save
+            display.value.classList.add("mydisplay");
         } else if (detail.target.value === "AC") {
-            display.value = " ";
+            display.value = "";
         } else if (detail.target.value === "Del") {
             display.value = display.value.slice(0, -1);
         }
         else {
             display.value += detail.target.value;
         }
-
     })
 });
